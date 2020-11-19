37ebd494eaa68c6d5b2daa6bcbf94fb4b44b19bb
anthony1313 <anthony.1960@hotmail.es>
2020-11-18 22:04:02 -0600

    frames1

--- Grey-Sloan Memorial Hospital/Hospital/frmEnfermera.Designer.cs
+++ Grey-Sloan Memorial Hospital/Hospital/frmEnfermera.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hospital
 {
-    partial class frmDoctor
+    partial class frmEnfermera
     {
         /// <summary>
         /// Required designer variable.
@@ -28,207 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.cbxEspecialidad = new System.Windows.Forms.ComboBox();
             this.txtCedula = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtApellido1 = new System.Windows.Forms.TextBox();
             this.txtApellido2 = new System.Windows.Forms.TextBox();
             this.txtId = new System.Windows.Forms.TextBox();
-            this.dgvDoctores = new System.Windows.Forms.DataGridView();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDoctores)).BeginInit();
+            this.dgvEnfermera = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnfermera)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(393, 154);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(119, 31);
+            this.btnEditar.TabIndex = 43;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(639, 154);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(119, 31);
+            this.btnEliminar.TabIndex = 42;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(101, 154);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(119, 31);
+            this.btnAgregar.TabIndex = 41;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(23, 107);
+            this.label6.Location = new System.Drawing.Point(30, 107);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(56, 17);
-            this.label6.TabIndex = 24;
+            this.label6.TabIndex = 40;
             this.label6.Text = "Cedula:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(311, 34);
+            this.label5.Location = new System.Drawing.Point(318, 34);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 17);
-            this.label5.TabIndex = 23;
+            this.label5.TabIndex = 39;
             this.label5.Text = "Nombre:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(311, 107);
+            this.label4.Location = new System.Drawing.Point(318, 107);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(70, 17);
-            this.label4.TabIndex = 22;
+            this.label4.TabIndex = 38;
             this.label4.Text = "Apellido1:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(535, 34);
+            this.label3.Location = new System.Drawing.Point(542, 34);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 17);
-            this.label3.TabIndex = 21;
+            this.label3.TabIndex = 37;
             this.label3.Text = "Apellido2";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(535, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 17);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "Especialidad:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Location = new System.Drawing.Point(30, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 17);
-            this.label1.TabIndex = 19;
+            this.label1.TabIndex = 35;
             this.label1.Text = "ID:";
-            // 
-            // cbxEspecialidad
-            // 
-            this.cbxEspecialidad.FormattingEnabled = true;
-            this.cbxEspecialidad.Location = new System.Drawing.Point(632, 100);
-            this.cbxEspecialidad.Name = "cbxEspecialidad";
-            this.cbxEspecialidad.Size = new System.Drawing.Size(121, 24);
-            this.cbxEspecialidad.TabIndex = 18;
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(94, 100);
+            this.txtCedula.Location = new System.Drawing.Point(101, 100);
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(119, 22);
-            this.txtCedula.TabIndex = 17;
+            this.txtCedula.TabIndex = 33;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(386, 29);
+            this.txtNombre.Location = new System.Drawing.Point(393, 29);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(119, 22);
-            this.txtNombre.TabIndex = 16;
+            this.txtNombre.TabIndex = 32;
             // 
             // txtApellido1
             // 
-            this.txtApellido1.Location = new System.Drawing.Point(386, 100);
+            this.txtApellido1.Location = new System.Drawing.Point(393, 100);
             this.txtApellido1.Name = "txtApellido1";
             this.txtApellido1.Size = new System.Drawing.Size(119, 22);
-            this.txtApellido1.TabIndex = 15;
+            this.txtApellido1.TabIndex = 31;
             // 
             // txtApellido2
             // 
-            this.txtApellido2.Location = new System.Drawing.Point(632, 29);
+            this.txtApellido2.Location = new System.Drawing.Point(639, 29);
             this.txtApellido2.Name = "txtApellido2";
             this.txtApellido2.Size = new System.Drawing.Size(119, 22);
-            this.txtApellido2.TabIndex = 14;
+            this.txtApellido2.TabIndex = 30;
             // 
             // txtId
             // 
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(94, 34);
+            this.txtId.Location = new System.Drawing.Point(101, 34);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(119, 22);
-            this.txtId.TabIndex = 13;
+            this.txtId.TabIndex = 29;
             // 
-            // dgvDoctores
+            // dgvEnfermera
             // 
-            this.dgvDoctores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDoctores.Location = new System.Drawing.Point(12, 204);
-            this.dgvDoctores.Name = "dgvDoctores";
-            this.dgvDoctores.RowHeadersWidth = 51;
-            this.dgvDoctores.RowTemplate.Height = 24;
-            this.dgvDoctores.Size = new System.Drawing.Size(776, 234);
-            this.dgvDoctores.TabIndex = 25;
+            this.dgvEnfermera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEnfermera.Location = new System.Drawing.Point(12, 204);
+            this.dgvEnfermera.Name = "dgvEnfermera";
+            this.dgvEnfermera.RowHeadersWidth = 51;
+            this.dgvEnfermera.RowTemplate.Height = 24;
+            this.dgvEnfermera.Size = new System.Drawing.Size(776, 234);
+            this.dgvEnfermera.TabIndex = 44;
             // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(94, 159);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(119, 31);
-            this.btnAgregar.TabIndex = 26;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(632, 159);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(119, 31);
-            this.btnEliminar.TabIndex = 27;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.Location = new System.Drawing.Point(386, 159);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(119, 31);
-            this.btnEditar.TabIndex = 28;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = true;
-            // 
-            // frmDoctor
+            // frmEnfermera
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvEnfermera);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.dgvDoctores);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.cbxEspecialidad);
             this.Controls.Add(this.txtCedula);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.txtApellido1);
             this.Controls.Add(this.txtApellido2);
             this.Controls.Add(this.txtId);
-            this.Name = "frmDoctor";
-            this.Text = "Doctores";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDoctores)).EndInit();
+            this.Name = "frmEnfermera";
+            this.Text = "Enfermera";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEnfermera)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox cbxEspecialidad;
         private System.Windows.Forms.TextBox txtCedula;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtApellido1;
         private System.Windows.Forms.TextBox txtApellido2;
         private System.Windows.Forms.TextBox txtId;
-        private System.Windows.Forms.DataGridView dgvDoctores;
-        private System.Windows.Forms.Button btnAgregar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.DataGridView dgvEnfermera;
     }
 }
-
